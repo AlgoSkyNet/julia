ebd6af5dbc71f0f83becb57dd64bd9ea866a959d
Simon Byrne <simonbyrne@gmail.com>
2016-11-10 09:20:46 +0000

    add NEWS.md item

--- base/deprecated.jl
+++ base/deprecated.jl
@@ -1109,6 +1109,7 @@ end)
 @deprecate den denominator
 @deprecate num numerator
 
+# #19288
 eval(Base.Dates, quote
     function recur{T<:TimeType}(fun::Function, dr::StepRange{T};negate::Bool=false, limit::Int=10000)
         depwarn("Dates.recur is deprecated, use filter instead.",:recur)
